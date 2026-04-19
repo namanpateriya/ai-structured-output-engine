dd40dc3463fd4a20ab6f08023fb896354ca25181
namanpateriya <pateriyanaman@gmail.com>
2026-04-19 12:45:41 +0530

    Update logger.py with issue fix

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,8 +1,12 @@
 import logging
 
 def get_logger(name: str):
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
-    )
-    return logging.getLogger(name)
+    logger = logging.getLogger(name)
+
+    if not logging.getLogger().hasHandlers():
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+        )
+
+    return logger
